e37d62365428f169ea6d9c17ad51ace8bd403389
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2024-05-09 11:27:56 +0900

    update

--- web_0509/app.py
+++ web_0509/app.py
@@ -1,5 +1,10 @@
 # flask 웹 프레임워크를 로드
-from flask import Flask
+# render_template() -> html문서를 유저에게 되돌려주기 위한 함수
+#   templates 폴더 안에 있는 html 문서를 되돌려준다
+#   html문서를 parsing 작업을 통해 유저에게 되돌려주는 형태
+#   html 문서에서 {{변수명}}, {% python code %}를 사용 가능
+#   f-string과 비슷한 기능
+from flask import Flask, render_template
 
 # Flask class 생성
 # 생성자 함수에 필수 인자 값 : 1개
@@ -13,7 +18,18 @@ app = Flask(__name__)
 # ex) @app.route('/view_info') --> localhost:8080/view_info 주소로 요청시
 @app.route('/')
 def index():
-    return 'Hello World'
+    return '<h1>Hello World</h1>'
+
+# 새로운 api 생성 
+# page1.html을 되돌려주는 api
+@app.route('/first')
+def first():
+    # templates 폴더 안에 있는 page1.html 파일을 로드하여 
+    # parsing
+    print(render_template('page1.html', name = 'test'))
+    print(type(render_template('page1.html', name = 'test')))
+    return render_template('page1.html', name = 'test') 
+
 
 
 # 웹 서버를 실행
