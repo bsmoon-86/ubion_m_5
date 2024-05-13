be2f7747ea3a8673690c4cd9041a1dc1ad4825ea
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2024-05-13 11:35:14 +0900

    update

--- ajax_server/app.py
+++ ajax_server/app.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# form 태그를 이용하여 보낸 데이터를 받는 api
+@app.route('/data')
+def data():
+    # get방식으로 들어온 데이터를 확인
+    req = request.args
+    print("form 태그를 이용한 요청 메시지 :", req)
+    return ""
+
+# ajax를 이용하여 보낸 데이터를 받는 api 
+@app.route("/ajax_data")
+def ajax_data():
+    # get방식으로 들어온 데티터를 확인
+    req = request.args 
+    print("ajax를 이용한 요청 메시지 :", req)
+    return "aaa"
+
 
 app.run(debug=True)
