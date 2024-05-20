82ac384e310a82094760959c4b3b63c2a4e5dc0b
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2024-05-20 14:09:59 +0900

    update

--- web_service/img_slide/app.py
+++ web_service/img_slide/app.py
@@ -16,7 +16,25 @@ mydb = MyDB(
 # localhost:5000/ api을 생성하여 img_slide.html를 되돌려주는 함수 생성
 @app.route('/')
 def slide():
-    return render_template('img_slide.html')
+    # DB server에 있는 company_list table의 정보를 로드 
+    select_query = """
+        select * from company_list
+    """
+    # class 생성한 mydb안에 내장된 함수를 호출
+    db_data = mydb.db_execute(select_query)
+    # db_data의 타입은? [{name : xxxx, img_url : xxxxx, link_url : xxxxx}, {}]
+    # db_data에서 img_url만 따로 추출
+    imgs = []
+    links = []
+    for data in db_data:
+        imgs.append(data['img_url'])
+        links.append(data['link_url'])
+    print(imgs)
+    print(links)
+    return render_template('img_slide.html', 
+                           imgs = imgs, 
+                           links = links, 
+                           cnt = len(imgs))
 
 # 웹서버를 실행
 app.run(debug=True)
